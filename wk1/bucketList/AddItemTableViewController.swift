6f5a32faff6b70c0e69024767c65c25d8c07c287
Kristopher Merolla <kristopher.merolla@gmail.com>
2017-07-10 14:40:09 -0700

    Cancel and Save functionality for bucket list app

--- wk1/bucketList/AddItemTableViewController.swift
+++ wk1/bucketList/AddItemTableViewController.swift
@@ -2,7 +2,7 @@
 //  AddItemTableViewController.swift
 //  bucketList
 //
-//  Created by Kristopher Merolla on 7/7/17.
+//  Created by Kristopher Merolla on 7/10/17.
 //  Copyright © 2017 Kristopher Merolla. All rights reserved.
 //
 
@@ -10,12 +10,30 @@ import UIKit
 
 class AddItemTableViewController: UITableViewController {
     
+    // setup delegate so the CancelButtonDelegate.swift functions can be run
+    weak var delegate: AddItemTableViewControllerDelegate?
+    @IBOutlet weak var itemTextField: UITextField!
+
+    // button press functions
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        // call the function on the delage swift file
+        delegate?.cancelButtonPressed(by: self)
+    }
+    @IBAction func saveButtonPressed(_ sender: UIBarButtonItem) {
+        let text = itemTextField.text! // force unwrap
+        delegate?.itemSaved(by: self, with: text)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+
     }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
-    
+
+
+
 }
--- wk1/bucketList/bucketList/BucketListViewController.swift
+++ wk1/bucketList/bucketList/BucketListViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class BucketListViewController: UITableViewController {
+class BucketListViewController: UITableViewController, AddItemTableViewControllerDelegate {
     
     var items = ["Go to the moon","Eat a candy bar","Swim in the Amazon","Ride a motor bike in Tokyo"]
 
@@ -33,6 +33,30 @@ class BucketListViewController: UITableViewController {
         return cell
     }
 
+    // need to override the prepare for segue function
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        let navigationController = segue.destination as! UINavigationController
+        let addItemTableViewController = navigationController.topViewController as! AddItemTableViewController
+        addItemTableViewController.delegate = self
+        // need to add the CancelButtonDelegate to the class line
+    }
+    // add the function that lets this file be a cancel button delegate
+    func cancelButtonPressed(by controller: AddItemTableViewController) {
+        print("I'm the hidden controller BUT I am responding to the cancel button press on the top view controller")
+        // remove top view controller
+        dismiss(animated: true, completion: nil)
+    }
+    // add item
+    func itemSaved(by controller: AddItemTableViewController, with text: String) {
+        print("text from top view recieved \(text)")
+        // add the item (from text box) into table view array
+        items.append(text)
+        // reload table view
+        tableView.reloadData()
+        // remove top view controller
+        dismiss(animated: true, completion: nil)
+    }
 
 }
 
