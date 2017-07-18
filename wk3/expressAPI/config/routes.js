b8734e9e121209c32d8a5b5545daab897f0777ef
Kristopher Merolla <kristopher.merolla@gmail.com>
2017-07-18 15:17:56 -0700

    Modified routing

--- wk3/expressAPI/config/routes.js
+++ wk3/expressAPI/config/routes.js
@@ -5,7 +5,7 @@ var router = express.Router()
 var Task = require('../models/Task')
 
 // Helper function responsible for fetching all Tasks
-function getAllTasks(req, res){
+function getAllTasks(req, res) {
   Task.find({}, function (err, tasks) {
     if (err) {
         console.error(`Error: ${error}`)
@@ -17,13 +17,31 @@ function getAllTasks(req, res){
   })
 }
 
-router.route('/tasks')
-  .get(function (req, res) {
+function postNewTask(req, res) {
+  // console.log("inside the postNewTask!")
+  console.log(req.body.objective);
+  // console.log("end of body post")
+  var task = new Task({objective: req.body.objective});
+  task.save(function(err) {
+    if (err) {
+      return err;
+    }
+    else {
+      console.log("Task Saved")
+    }
+  })
+}
+
+router.route('/tasks').get(function (req, res) {
   /** 
   * Respond with all tasks stored on database if 'GET' request to route 
   * '/tasks'; the only time we send an error back to the client
   */
     getAllTasks(req, res)
+  })
+
+router.route('/tasks').post(function (req,res) {
+    postNewTask(req, res)
   })
 
 module.exports = router
